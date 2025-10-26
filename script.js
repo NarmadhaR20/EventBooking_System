98a777a67e0320ead8b0905a7bf98f8e3ae6cf9d
NarmadhaR20 <140188825+NarmadhaR20@users.noreply.github.com>
2025-10-26 15:29:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+var Typed=new Typed(".text", {
+    strings: ["EventEase"],
+    typeSpeed: 100,
+    backSpeed: 150,
+    backDelay: 1500,
+    loop: true,
+  });
+
+var typed1=new Typed(".browseone", {
+    strings: ["Book Theatre Shows"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
+
+var Typed2=new Typed(".browsetwo", {
+    strings: ["Book Concerts Events"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
+
+var Typed3=new Typed(".browsethree", {
+    strings: ["Book Sports Events"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
+var typed1=new Typed(".browsefour", {
+    strings: ["Book Laughter Therapy"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
